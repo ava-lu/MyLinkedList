a5d0965d77f61b4659d7414de583b621a4267aa3
ava-lu <alu21@stuy.edu>
2021-01-06 13:51:18 -0500

    added get/set for next

--- Node.java
+++ Node.java
@@ -10,4 +10,10 @@ public class Node {
   public void setData(String str) {
     data = str;
   }
+  public Node getNext() {
+    return next;
+  }
+  public void setNext(Node n) {
+    next = n;
+  }
 }
