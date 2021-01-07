00cc6237035debd60dacd8fe8f8c5dd66ead6928
ava-lu <alu21@stuy.edu>
2021-01-06 23:16:55 -0500

    added void add() and a helper function

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -12,15 +12,37 @@ public class MyLinkedList {
     if (size==0) {
       start = a;
       end = a;
-      size++;
-      return true;
     }
     else {
       end.setNext(a);
       a.setPrev(end);
       end = a;
-      size++;
-      return true;
     }
+    size++;
+    return true;
+  }
+  private Node getIndex(int index) {
+    Node ans = start;
+    for (int i=0; i<index; i++) {
+      ans = ans.getNext();
+    }
+    return ans;
+  }
+  public void add(int index, String value) {
+    if (index>size) {
+      throw new IndexOutOfBoundsException();
+    }
+    Node current = getIndex(index);
+    Node adding = new Node(value);
+    adding.setNext(current);
+    adding.setPrev(current.getPrev());
+    if (index==0) {
+      start = current;
+    }
+    else {
+      current.getPrev().setNext(adding);
+      current.setPrev(adding);
+    }
+    size++;
   }
 }
