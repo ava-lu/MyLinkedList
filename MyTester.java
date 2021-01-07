749ecc5cabf1032b576cc6e1a22e9c476e3c04f7
ava-lu <alu21@stuy.edu>
2021-01-06 23:46:28 -0500

    tested size

---
+++ MyTester.java
@@ -0,0 +1,9 @@
+public class MyTester {
+  public static void main(String[] args) {
+    //size test
+    MyLinkedList test = new MyLinkedList();
+    test.add("Beep");
+    test.add("Boop");
+    System.out.println(test.size());
+  }
+}
