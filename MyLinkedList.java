be18141b5bfca29705fc0575a5f5073c100dda36
ava-lu <alu21@stuy.edu>
2021-01-06 22:15:57 -0500

    added boolean add()

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,4 +7,18 @@ public class MyLinkedList {
   public int size() {
     return size;
   }
+  public boolean add(String value) {
+    Node a = new Node(value);
+    if (size==0) {
+      start = a;
+      end = a;
+      return true;
+    }
+    else {
+      end.setNext(a);
+      a.setPrev(end);
+      end = a;
+      return true;
+    }
+  }
 }
